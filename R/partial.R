ba303a6e49b958c31be00567482043141754ea4e
Brandon Greenwell <greenwell.brandon@gmail.com>
2015-12-29 10:41:49 -0500

    swithed to using plyr for partial functions

--- R/partial.R
+++ R/partial.R
@@ -9,32 +9,34 @@
 #' @param n Integer giving the number of unique data points to use in
 #'   computing the partial dependence values.
 #' @param newdata An optional data frame.
+#' @param ... Additional optional arguments passed onto \code{aaply}.
+#' @importFrom plyr laply
 #' @export
-partial_1d <- function(object, x.name, n, newdata) {
-
+partial_1d <- function(object, x.name, n, newdata, ...) {
+  
   stopifnot(inherits(object, "mertree"))
-
+  
   # Data frame
   .data <- if (missing(newdata)) eval(object$call$data) else newdata
-
+  
   # Sorted unique values of the independent variable
   sux <- sort(unique(.data[[x.name]]))
   if (!missing(n)) {
     sux <- seq(from = min(sux), to = max(sux), length = n)
   }
-
+  
   # Compute average prediction for each unique value
-  pd <- sapply(sux, FUN = function(x) {
+  pd <- laply(sux, .fun = function(x) {
     temp <- .data
     temp[[x.name]] <- x
     mean(predict(object, newdata = temp))
-  })
+  }, ...)
 
   # Return data frame of partial dependence values
   pd_df <- data.frame(x = sux, y = pd)
   names(pd_df) <- c(x.name, "y")
   pd_df
-
+  
 }
 
 
@@ -53,40 +55,41 @@ partial_1d <- function(object, x.name, n, newdata) {
 #' @param n2 Integer giving the number of unique data points to use in
 #'   computing the partial dependence values for \code{x2.name}.
 #' @param newdata An optional data frame.
+#' @param ... Additional optional arguments passed onto \code{aaply}.
+#' @importFrom plyr aaply
 #' @export
-partial_2d <- function(object, x1.name, x2.name, n1, n2, newdata) {
-
+partial_2d <- function(object, x1.name, x2.name, n1, n2, newdata, ...) {
+  
   stopifnot(inherits(object, "mertree"))
-
+  
   # Data frame
   .data <- if (missing(newdata)) eval(object$call$data) else newdata
-
+  
   # Sorted unique values of the first independent variable
   sux1 <- sort(unique(.data[[x1.name]]))
   if (!missing(n1)) {
     sux1 <- seq(from = min(sux1), to = max(sux1), length = n1)
   }
-
+  
   # Sorted unique values of the second independent variable
   sux2 <- sort(unique(.data[[x2.name]]))
   if (!missing(n2)) {
     sux2 <- seq(from = min(sux2), to = max(sux2), length = n2)
   }
-
+  
   # Data frame of unique combinations
   xgrid <- expand.grid("x1" = sux1, "x2" = sux2)
-
+  
   # Compute average prediction for each unique value
-  pd <- apply(xgrid, 1, function(x) {
+  pd_df <- adply(xgrid, .margins = 1, .fun = function(x) {
     temp <- .data
-    temp[[x1.name]] <- x[1]
-    temp[[x2.name]] <- x[2]
+    temp[[x1.name]] <- x[[1]]
+    temp[[x2.name]] <- x[[2]]
     mean(predict(object, newdata = temp))
-  })
+  }, ...)
 
   # Return data frame of partial dependence values
-  pd_df <- cbind(xgrid, pd)
   names(pd_df) <- c(x1.name, x2.name, "y")
   pd_df
-
+  
 }
