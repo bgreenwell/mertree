f52607e523cadc94ab78bf39cb10dcc529589ffb
bgreenwell <greenwell.brandon@gmail.com>
2016-01-12 20:31:22 -0500

    added data set

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Simulated Data
+#'
+#' The data are described in [REF1] and (originally) in[REF2].
+#'
+#' \itemize{
+#'   \item \code{subject} Subject ID.
+#'   \item \code{x1} First predictor variable.
+#'   \item \code{x2} Second predictor variable.
+#'   \item \code{x3} Third predictor variable.
+#'   \item \code{x4} Fourth predictor variable.
+#'   \item \code{x5} Fifth predictor variable.
+#'   \item \code{x6} Sixth predictor variable.
+#'   \item \code{time} Time variable.
+#'   \item \code{y} Response variable.
+#' }
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 500 rows and 9 variables
+#' @name simd
+#' @source
+#' [REF1]
+#' [REF2]
+NULL
