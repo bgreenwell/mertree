316df9289ba8d67995a2c3e5e7d11e013bc1bb93
bgreenwell <greenwell.brandon@gmail.com>
2016-01-16 13:18:45 -0500

    mertree uses lme4::lmer error mssg when no re terms are present

--- R/formula.R
+++ R/formula.R
@@ -30,7 +30,12 @@ get_fixed_formula <- function(formula) {
 #' @importFrom lme4 findbars
 #' @export
 get_random_formula <- function(formula) {
-  paste0("(", findbars(formula), ")", collapse = " + ")
+  form <- paste0("(", findbars(formula), ")", collapse = " + ")
+  if (form == "()") {
+    NULL
+  } else {
+    form
+  }
 }
 
 
@@ -47,7 +52,7 @@ get_random_formula <- function(formula) {
 #'
 #' @export
 make_lmer_formula <- function(response, fixed, random) {
-  if (missing(random)) {
+  if (missing(random) || is.null(random)) {
     formula(paste(response, fixed, sep = " ~ "))
   } else {
     formula(paste(response, paste(fixed, random, sep = " + "), sep = " ~ "))
